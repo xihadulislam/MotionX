4ed366b8e154ec26445cd3c999484200cf7fb223
ziad <ATBBE8AgWgth8TTLjK6as8rSD48L8A1FAC99>
2023-03-28 13:27:05 +0600

    init

---
+++ app/src/main/java/com/xihadulislam/motionx/step_to_follow/OnClickMotionActivity.kt
@@ -0,0 +1,12 @@
+package com.xihadulislam.motionx.step_to_follow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xihadulislam.motionx.R
+
+class OnClickMotionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_click_motion)
+    }
+}
